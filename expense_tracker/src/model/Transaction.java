9e50aa42168242add8a453936b355a6cfeb708f7
moumitakarmakar123 <moumitakarmakar.work@gmail.com>
2025-10-29 18:33:25 +0530

    Model Files

---
+++ expense_tracker/src/model/Transaction.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class Transaction {
+
+  private final double amount;
+  private final String category;
+  private final String timestamp;
+
+  public Transaction(double amount, String category) {
+    this.amount = amount;
+    this.category = category;
+    this.timestamp = generateTimestamp();
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public String getCategory() {
+    return category;
+  }
+  
+  public String getTimestamp() {
+    return timestamp;
+  }
+
+  private String generateTimestamp() {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");  
+    return sdf.format(new Date());
+  }
+
+}
